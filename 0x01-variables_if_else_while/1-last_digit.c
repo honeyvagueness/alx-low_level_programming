1738442a68bd4087c275b363591310cf180b15ef
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-16 12:25:50 -0700

    klo

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success
+ */
+
+int main(void)
+
+{
+
+char alphabet = 'a';
+
+char tyo = '\n';
+
+while (alphabet <= 'z')
+
+{
+
+putchar(alphabet);
+
+alphabet++;
+
+}
+
+putchar(tyo);
+
+return (0);
+
+}

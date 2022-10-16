20e78f8bf5aaf19215ea706acd851f2056952c64
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-16 14:15:17 -0700

    woyon

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+int var = 48;
+
+int gar = '\n';
+
+while (var < 58)
+
+{
+
+putchar(var);
+
+var++;
+
+}
+
+putchar(gar);
+
+return (0);
+
+}

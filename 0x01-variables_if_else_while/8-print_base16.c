9ae638c694af28d6c3b0ae059d3b45b3a9b632ae
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-16 14:23:49 -0700

    ban

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+int var = 48;
+
+char let = 'a';
+
+char tyo = '\n';
+
+while (var < 58)
+
+{
+
+putchar(var);
+
+var++;
+
+}
+
+while (let <= 'f')
+
+{
+
+putchar(let);
+
+let++;
+
+}
+
+putchar(tyo);
+
+return (0);
+
+}

07bdaab8e9ed58e93a25554464b10f16c5198661
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-16 14:19:24 -0700

    cayon

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+char alphabet = 'z';
+
+char tyo = '\n';
+
+while (alphabet >= 'a')
+
+{
+
+putchar(alphabet);
+
+alphabet--;
+
+}
+
+putchar(tyo);
+
+return (0);
+
+}

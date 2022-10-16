8478f4e442020ee996c3d5bb2e3f953fee991d86
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-16 14:10:12 -0700

    monday

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+int var = 0;
+
+while (var < 10)
+
+{
+
+printf("%d", var);
+
+var++;
+
+}
+
+printf("\n");
+
+return (0);
+
+}

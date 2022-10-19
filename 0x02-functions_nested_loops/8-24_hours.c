98452993c8abbb8f3fa8582b5409ce8c5eca63df
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-19 08:35:08 -0700

    jacl

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/**
+
+/*
  * jack_bauer - prints every minute of the day
  * Description: prints every minute of the day
  * Return: void
@@ -7,14 +8,13 @@
 void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
-	hours_max = 58;
 
+	hours_max = 58;
 	hours_tens = '0';
 	while (hours_tens < '3')
 	{
 		if (hours_tens == '2')
 		{
-
 			hours_max = '4';
 		}
 		hours_ones = '0';

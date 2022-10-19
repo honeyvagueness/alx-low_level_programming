88ee706ed4f17b5ffbae94b1b9e25da9643ef347
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-19 10:03:13 -0700

    dm

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,17 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 int main(void)
+
 {
+
     times_table();
+
     return (0);
+
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,41 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table
- * Description: prints the 9 times table
- * Return: void
+ * times_table - Prints the 9 times table, starting with 0
  */
+
 void times_table(void)
+
 {
-	int row, column, product, tens, ones;
-	for (row = 0; row <= 9; row++)
+
+	int i, j, n;
+
+
+
+	for (i = 0; i <= 9; i++)
+
 	{
-		for (column = 0; column <= 9; column++)
+		for (j = 0; j <= 9; j++)
 		{
-			product = row * column;
-			tens = product / 10;
-			ones = product % 10;
-			if (column == 0)
+			n = i * j;
+			if (n > 9)
 			{
-				_putchar('0');
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
 			}
-			else if (product < 10)
+			else if (j != 0)
 			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(ones + '0');
+				_putchar(n + '0');
 			}
 			else
 			{
+				_putchar(n + '0');
+			}
+			if (j != 9)
+			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
 			}
 		}
 		_putchar('\n');

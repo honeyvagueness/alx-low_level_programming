d522f4af969c8475624fb14ba37f9b914965ad34
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-19 07:46:15 -0700

    fixes

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,6 +9,7 @@ int _abs(int a)
 {
 	int k = a;
 	int absvalue;
+
 	if (k < 0)
 	{
 		k = k * (-1);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_last_digit - prints the last digit of a number
  * @n: n is an integer
@@ -6,9 +7,11 @@
  * Return: integer
  */
 int print_last_digit(int n)
+
 {
 	int last = n % 10;
 	if (n < 0)
+
 	{
 		last = last * -1;
 	}

0b8fa82c2b1b819e109231a2d4496a9a5c0abcd7
honeyvagueness <olaniyanandrew5@gmail.com>
2022-10-19 06:16:22 -0700

    bn

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
+ * Return: On success 1.
+ */
+void print_alphabet_x10(void)
+{
+int spiro = 48;
+int mor = '\n';
+while (spiro < 58)
+{
+int ally = 97;
+int pull = 122;
+while (ally <= pull)
+{
+putchar(ally);
+ally++;
+}
+spiro++;
+putchar(mor);
+}
+}
